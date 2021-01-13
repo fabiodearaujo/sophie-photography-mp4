d82bfa92ba0ea4e69dad5e4c4c27b1d1df74a98f
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-01-13 00:59:56 +0000

    Created env.py file including Secret Key and updated settings.py to read it. Also included a verification so it will work in the development environment and when it is deployed

--- sophie_photo/settings.py
+++ sophie_photo/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(&j41&9*#_yjjir@y^(r$uaqb_imfhcanqq-zb76x)%sn4p684'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
