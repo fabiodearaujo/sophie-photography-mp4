1c3f5cea03e48f3cc06ff3249bbe3eb51bd33da4
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-01-14 00:19:53 +0000

    Created templates folder on home app with index.html and updated views, urls and settings to make the conneciton close #7

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ View to connect and return the index page """
+    return render(request,'home/index.html')
