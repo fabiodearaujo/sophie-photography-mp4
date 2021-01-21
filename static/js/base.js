fd51a3545d82e111407a6eb55075db1b936bca34
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-01-21 01:15:44 +0000

    improved landing page, included lax js for effects and created About part close #10

---
+++ static/js/base.js
@@ -0,0 +1,11 @@
+
+// Parallax initiation 
+window.onload = function () {
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+}
