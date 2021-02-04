3d99dd327c1a0dddd4bae7b5cb6984a914a4db84
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-04 00:44:02 +0000

    Added the search functionality on navbar

--- products/views.py
+++ products/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 def products(request):
     """ A view to show the product, including sorting and searching """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                message.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request,'products/products.html', context)
