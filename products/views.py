5c8001c0158f8426e2533dbec4233fe1b5df74ed
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-05 00:17:16 +0000

    Added sorting capacity to the packages and products page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from .models import Product
+from .models import Product, Category
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q
 
 # Create your views here.
@@ -8,8 +9,24 @@ def products(request):
     """ A view to show the product, including sorting and searching """
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -18,11 +35,14 @@ def products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
 
 
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request,'products/products.html', context)
