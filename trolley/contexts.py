360be3de4b8059110939d34303697ddd8356f4cf
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-11 01:43:24 +0000

    created context to keep track on Trolley contents and updates navbar to show values

--- trolley/contexts.py
+++ trolley/contexts.py
@@ -1,10 +1,25 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def trolley_contents(request):
 
     trolley_items = []
     total = 0
     product_count = 0
+    trolley_dic = request.session.get('trolley', {})
+    print(trolley_dic)
+
+    for item_id, quantity in trolley_dic.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        trolley_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'trolley_items': trolley_items,
