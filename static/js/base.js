e41a954dcf9fc90c222d5580f3c8c263b62e6392
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-12 01:59:19 +0000

    Improved glassify effect and fixed Navbar so it is now transparent at first, other CSS colour styles fixed

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,7 @@
 
 
 //Function based on the video from Kevin Powell - https://www.youtube.com/watch?v=SmolT-tV5Lw&t=945s
+opacityValue = 0;
 
 $(window).scroll(function() {
     parallax();
@@ -9,11 +10,14 @@ $(window).scroll(function() {
 function parallax() {
     var wScroll = $(window).scrollTop();
 
-    $('.nav-glassify').css('opacity', ((wScroll/4)+60)+'%');
+    $('.nav-glassify').css('background-color', 'hsla(204, 97%, 64%, '+((wScroll/800)+0.01)+")");
 
     $('.parallax-bg').css('background-position', 'center '+(wScroll*(-0.2))+'px');
 
     $('.parallax-line').css('margin-top', (wScroll*0.6)+'px');
     $('.parallax-line').css('opacity', (100-(wScroll/2))+'%');
+
+    $('.parallax-scroll').css('margin-top', (wScroll*0.07)+'px');
+    $('.parallax-scroll').css('opacity', (100-(wScroll/2))+'%');
 
 }
