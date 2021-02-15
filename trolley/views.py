7e60d3a638bda32f3e71883bc2033f9b81529c2c
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-15 01:17:25 +0000

    added functionality of adjusting quantity or removing products on Shopping trolley

--- trolley/views.py
+++ trolley/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 # Create your views here.
@@ -22,3 +22,33 @@ def add_to_trolley(request, item_id):
 
     request.session['trolley'] = trolley
     return redirect(redirect_url)
+
+def adjust_trolley(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    trolley = request.session.get('trolley', {})
+    
+    if quantity > 0:
+        if quantity < 100:
+            trolley[item_id] = quantity
+        else:
+            trolley[item_id] =  99
+    else:
+        trolley.pop(item_id)
+
+    request.session['trolley'] = trolley
+    return redirect(reverse('view_trolley'))
+
+
+def remove_from_trolley(request, item_id):
+    """Remove the item from the shopping trolley"""
+
+    try:
+        trolley = request.session.get('trolley', {})
+        trolley.pop(item_id)
+        request.session['trolley'] = trolley
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
