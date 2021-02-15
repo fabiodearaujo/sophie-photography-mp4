a3e2e520afb5ef6ea682cf9999f2aed7011d489d
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-15 22:47:00 +0000

    Included handler to different types of message and removed part of the styling stucture of the toast to adapt to my layout

--- trolley/views.py
+++ trolley/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect, reverse, HttpResponse
 from django.contrib import messages
 from products.models import Product
 
@@ -11,7 +11,7 @@ def view_trolley(request):
 def add_to_trolley(request, item_id):
     """ Add a quantity of the specified product to the shopping trolley """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     trolley = request.session.get('trolley', {})
@@ -20,7 +20,7 @@ def add_to_trolley(request, item_id):
         trolley[item_id] += quantity
     else:
         trolley[item_id] = quantity
-        messages.success(request, f'Added this {product.name} to the Trolley')
+        messages.success(request, f'Added {product.name} to the Trolley')
 
     request.session['trolley'] = trolley
     return redirect(redirect_url)
@@ -28,16 +28,20 @@ def add_to_trolley(request, item_id):
 def adjust_trolley(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     trolley = request.session.get('trolley', {})
     
     if quantity > 0:
         if quantity < 100:
             trolley[item_id] = quantity
+            messages.success(request, f'Quantity and Subtotal updated for {product.name}')
         else:
             trolley[item_id] =  99
+            messages.warning(request, 'Quantity cannot be higher than 99 items')
     else:
         trolley.pop(item_id)
+        messages.error(request, 'Item removed as the quantity was lower than 1 item')
 
     request.session['trolley'] = trolley
     return redirect(reverse('view_trolley'))
@@ -49,8 +53,10 @@ def remove_from_trolley(request, item_id):
     try:
         trolley = request.session.get('trolley', {})
         trolley.pop(item_id)
+        messages.success(request, 'The item was removed from your Trolley')
         request.session['trolley'] = trolley
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
