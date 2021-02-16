e2aa1a2544f29c62752232cac40fa0b686010fdc
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-16 22:48:33 +0000

    updated a typo on forms, updated link on trolley and changed styles on checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,7 +7,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'street_address', 'street_address2',
+                  'street_address1', 'street_address2',
                   'town_or_city', 'county', 'country',
                   'postcode',)
 
--- checkout/views.py
+++ checkout/views.py
@@ -5,9 +5,9 @@ from .forms import OrderForm
 
 
 def checkout(request):
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
+    trolley = request.session.get('trolley', {})
+    if not trolley:
+        messages.error(request, "There's nothing in your trolley at the moment")
         return redirect(reverse('products'))
 
     order_form = OrderForm()
