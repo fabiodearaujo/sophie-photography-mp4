2717e04d7e841aa9909b455dd2b62b07cf802a80
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-02-17 01:34:21 +0000

    Installed Stripe, updated requirements, included basic stripe checkout and changed styles on the checkout form to match stripe

--- sophie_photo/settings.py
+++ sophie_photo/settings.py
@@ -170,3 +170,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
